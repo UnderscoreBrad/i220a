6672ad77a3803d8b9a4ab51856080d51ab843828
Brad Estus <50963179+UnderscoreBrad@users.noreply.github.com>
2020-11-22 20:32:48 -0500

    fixed stack addressing issue

--- submit/prj4/prj4-sol/ysim.c
+++ submit/prj4/prj4-sol/ysim.c
@@ -28,7 +28,7 @@ static inline bool get_cc_flag(Byte cc, unsigned flagBitIndex) {
   return !!(cc & (1 << flagBitIndex)); //wtf is with the !! bro
 }
 static Byte set_cc_flags(unsigned zf_val, unsigned sf_val, unsigned of_val){
-  return ((zf_val<<ZF_CC) + (sf_val <<SF_CC) + (of_val<<OF_CC));  //This causes some (manageable) problems
+  return ((zf_val<<ZF_CC) + (sf_val <<SF_CC) + (of_val<<OF_CC));
 }
 static inline bool get_zf(Byte cc) { return get_cc_flag(cc, ZF_CC); }
 static inline bool get_sf(Byte cc) { return get_cc_flag(cc, SF_CC); }
@@ -64,7 +64,7 @@ check_cc(const Y86 *y86, Byte op)
     ret = (get_sf(cc) == get_of(cc)) ^ (get_zf(cc));
     break;
   case GT_COND:
-    ret = (get_of(cc)==get_sf(cc)) && (get_zf(cc)==0);
+    ret = (get_of(cc)==get_sf(cc)) && (get_zf(cc));
     break;
   default: {
     Address pc = read_pc_y86(y86);
@@ -87,7 +87,7 @@ isLt0(Word word) {
 static void
 set_add_arith_cc(Y86 *y86, Word opA, Word opB, Word result)
 {
-  Byte cc = 0;
+  Byte cc = 0;  //SF if result < 0, ZF if result == 0, OF if overflow
   if(result<0){
     cc = set_cc_flags(0,1,0);
   }
@@ -107,14 +107,14 @@ set_add_arith_cc(Y86 *y86, Word opA, Word opB, Word result)
 static void
 set_sub_arith_cc(Y86 *y86, Word opA, Word opB, Word result)
 {
-  Byte cc = 0;
+  Byte cc = 0; //SF if result < 0, ZF if result == 0, OF if overflow
   if(result<0){
    cc = set_cc_flags(0,1,0);
   }
   if(result == 0){
     cc = set_cc_flags(1,get_sf(cc),0);
   }
-  if((opA>0&&opB<0&&result<0)||(opA<0&&opB>0&&result>0)){
+  if((opB>0&&opA<0&&result>0)||(opB<0&&opA>0&&result<0)){
     cc = set_cc_flags(get_zf(cc),get_sf(cc),1);
   }
   write_cc_y86(y86, cc);
@@ -124,7 +124,7 @@ set_sub_arith_cc(Y86 *y86, Word opA, Word opB, Word result)
 static void
 set_logic_op_cc(Y86 *y86, Word result)
 {
-  Byte cc = 0;
+  Byte cc = 0; //SF if result < 0, ZF if result == 0, OF reset
   if(result<0){
     cc = set_cc_flags(0,1,0);
   }if(result == 0){
@@ -152,8 +152,8 @@ op1(Y86 *y86, Byte op, Register regA, Register regB)
     break;
   case SUBL_FN:
     result = valB - valA;   //OKAY SO THIS FRICKING LINE HERE
-    set_sub_arith_cc(y86, valA, valB, result); // ^  THAT LINE CAUSED THREE HOURS OF PROBLEMS
-    break;                    //I LITERALLY THOUGHT THE MRMOV INSTRUCTION WAS WRONG FOR 95% OF THAT TIME
+    set_sub_arith_cc(y86, valB, valA, result); // ^  THAT LINE CAUSED THREE HOURS OF PROBLEMS
+    break;                    //It still doesn't work right for abs-diff.ys
   case ANDL_FN:
     result = valA & valB;
     set_logic_op_cc(y86, result);  
@@ -232,6 +232,8 @@ step_ysim(Y86 *y86)
     Byte reg = get_nybble(read_memory_byte_y86(y86, pc+sizeof(Byte)),0);  //Find the destination register
     Word val = read_memory_word_y86(y86, pc+(2*sizeof(Byte)));  //Find the value of the word
     write_register_y86(y86, reg, val);  //Place word val into reg
+    //^This line doesn't get free'd
+    //but that's due to a problem in y86.c - write_register_y86 or add_change_y86.
     write_pc_y86(y86, pc+2*sizeof(Byte)+sizeof(Word)); //Increment PC
     break;
   case RMMOVQ_CODE: ;//OP 4
@@ -249,8 +251,8 @@ step_ysim(Y86 *y86)
     displacement = read_memory_word_y86(y86, pc+(2*sizeof(Byte)));
     valA = read_register_y86(y86, regB)+displacement;
     Word value = read_memory_word_y86(y86, valA);
-    write_register_y86(y86, regA, value);
-    write_pc_y86(y86, pc+(2*sizeof(Byte))+(sizeof(Word)));
+    write_register_y86(y86, regA, value); //This instruction writes to rA instead of rB
+    write_pc_y86(y86, pc+(2*sizeof(Byte))+(sizeof(Word))); //Because nothing can be easy
     break;
   case OP1_CODE: ;//OP 6 - dummy line to allow declarations
     Byte fn = get_nybble(op, 0);         
@@ -267,13 +269,13 @@ step_ysim(Y86 *y86)
     stackPtr = read_register_y86(y86, REG_RSP);
     Address rt = pc+sizeof(Byte)+sizeof(Word);
     Address go = read_memory_word_y86(y86, pc+sizeof(Byte));
-    write_memory_word_y86(y86, stackPtr, rt);
+    write_memory_word_y86(y86, stackPtr-sizeof(Word), rt);
     write_register_y86(y86, REG_RSP, stackPtr-sizeof(Word));
     write_pc_y86(y86, go);
     break;
   case RET_CODE: ;//OP 9
     stackPtr = read_register_y86(y86, REG_RSP);
-    Address r = read_memory_word_y86(y86, stackPtr+sizeof(Word));
+    Address r = read_memory_word_y86(y86, stackPtr);
     write_register_y86(y86, REG_RSP, stackPtr+sizeof(Word));
     write_pc_y86(y86, r);
     break;
@@ -281,14 +283,14 @@ step_ysim(Y86 *y86)
     regA = get_nybble(read_memory_byte_y86(y86, pc+sizeof(Byte)),1);
     valA = read_register_y86(y86, regA);
     stackPtr = read_register_y86(y86, REG_RSP);
-    write_memory_word_y86(y86, stackPtr, valA);
+    write_memory_word_y86(y86, stackPtr-sizeof(Word), valA);
     write_register_y86(y86, REG_RSP, stackPtr-sizeof(Word));
     write_pc_y86(y86, pc+(2*sizeof(Byte)));
   break;
   case POPQ_CODE: //OP B
     regA = get_nybble(read_memory_byte_y86(y86, pc+sizeof(Byte)),1);
     stackPtr = read_register_y86(y86, REG_RSP);
-    valA = read_memory_word_y86(y86, stackPtr+sizeof(Word));
+    valA = read_memory_word_y86(y86, stackPtr);//+sizeof(Word)
     write_register_y86(y86, REG_RSP, stackPtr+sizeof(Word));
     write_register_y86(y86, regA, valA); 
     write_pc_y86(y86, pc+(2*sizeof(Byte)));
